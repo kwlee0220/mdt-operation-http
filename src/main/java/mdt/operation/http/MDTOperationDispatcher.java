c12b234880e2701690e1f57dc64c4ee3f564311b
Kang-Woo Lee <kwlee@etri.re.kr>
2025-06-25 18:17:46 +0900

    ElementValue 인터페이스 수정.

--- src/main/java/mdt/operation/http/MDTOperationDispatcher.java
+++ src/main/java/mdt/operation/http/MDTOperationDispatcher.java
@@ -60,7 +60,6 @@ import mdt.model.sm.AASFile;
 import mdt.model.sm.ref.MDTElementReference;
 import mdt.model.sm.value.ElementValue;
 import mdt.model.sm.value.FileValue;
-import mdt.model.sm.value.PropertyValue;
 import mdt.model.sm.variable.AbstractVariable.ReferenceVariable;
 import mdt.model.sm.variable.Variable;
 import mdt.operation.http.program.ProgramOperationConfiguration;
@@ -420,9 +419,7 @@ public class MDTOperationDispatcher implements InitializingBean {
 				// PropertyValue인 경우, 바로 JSON으로 출력하면 double-quote가 추가되기 때문에
 				// 이를 막기 위해 값을 직접 저장한다.
 				file = new File(workingDir, variable.getName());
-				String extStr = (value instanceof PropertyValue pvalue)
-								? pvalue.get() : MDTModelSerDe.toJsonString(value);
-				IOUtils.toFile(extStr, StandardCharsets.UTF_8, file);
+				IOUtils.toFile(value.toValueJsonString(), StandardCharsets.UTF_8, file);
 				
 				return new FileVariable(variable.getName(), file);
 			}
